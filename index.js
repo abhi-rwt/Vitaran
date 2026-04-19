0e14592d3bf4097b515389d2e62e8adbac175a1f
Abhinav Rawat <abhinavrawat832006@gmail.com>
2026-04-19 22:56:08 +0530

    Fix: Increase payload limit for photo upload

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const userRoutes = require("./routes/user");
 const app = express();
 
 /* ===================== MIDDLEWARE ===================== */
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static(path.join(__dirname, "public")));
 
 /* ===================== ROOT ROUTE ===================== */
